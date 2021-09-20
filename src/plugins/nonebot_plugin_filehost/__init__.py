5ecea3845509ff017a939cd18448d6a13faf52da
Mix <mnixry@users.noreply.github.com>
2021-09-21 00:16:09 +0800

    :sparkles: Finish all expected FileHost feature

--- src/plugins/nonebot_plugin_filehost/__init__.py
+++ src/plugins/nonebot_plugin_filehost/__init__.py
@@ -1,79 +1,37 @@
-# import nonebot
+import atexit
+import shutil
 from contextvars import ContextVar
-from typing import Mapping, Optional, Sequence, Tuple
+from io import BufferedReader, BytesIO
+from pathlib import Path
+from secrets import token_urlsafe
+from tempfile import TemporaryDirectory
+from typing import Union
+from urllib.parse import ParseResult, urljoin
 
+from fastapi.staticfiles import StaticFiles
 from nonebot import get_driver
 from nonebot.drivers.fastapi import Driver as FastAPIDriver
 from nonebot.log import logger
-from pydantic import BaseModel, Extra, IPvAnyAddress
-from starlette.datastructures import Headers
+from nonebot.plugin.export import export
+from pydantic.main import BaseModel, Extra
 from starlette.types import ASGIApp, Receive, Scope, Send
-from typing_extensions import Literal
 
-from .config import Config
+from .config import Config, LinkType
+from .models import RequestScopeInfo
 
 driver = get_driver()
+
 
 if not isinstance(driver, FastAPIDriver):
     raise ValueError("Now filehost only support fastapi driver")
 
-config = Config.parse_obj(driver.config.dict())
+hosting_config = Config.parse_obj(driver.config.dict())
+export_namespace = export()
+
+temporary_dir = TemporaryDirectory(prefix="nonebot-filehost")
 
 
-# Export something for other plugin
-# export = nonebot.export()
-# export.foo = "bar"
-
-# @export.xxx
-# def some_function():
-#     pass
-
-
-class RequestHeaders(Headers):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, value):
-        if isinstance(value, Sequence) and all(
-            map(lambda e: isinstance(e, Sequence), value)
-        ):
-            return cls(raw=value)  # type:ignore
-        elif isinstance(value, Mapping):
-            if all(map(lambda k: isinstance(k, str), value.keys())) and all(
-                map(lambda v: isinstance(v, str), value.values())
-            ):
-                return cls(headers=value)
-            else:
-                return cls(scope=value)  # type:ignore
-        raise ValueError("Invalid request headers.")
-
-
-class RequestScopeInfo(BaseModel):
-    """
-    This is scope model for uvicorn
-
-    Reference: https://www.uvicorn.org/#http-scope
-    """
-
-    class Config:
-        extra = Extra.allow
-
-    asgi: Mapping[str, str]
-
-    type: Literal["http", "websocket"]
-    server: Tuple[IPvAnyAddress, int]
-    client: Tuple[IPvAnyAddress, int]
-
-    scheme: str
-    method: Optional[str] = None
-    http_version: Optional[str] = None
-    root_path: str
-    path: str
-    headers: RequestHeaders
-
-
+@export_namespace
 class HostContextVarMiddleware:
     current_scope: ContextVar[Scope] = ContextVar("current_scope")
     current_request: ContextVar[RequestScopeInfo] = ContextVar("current_request")
@@ -86,7 +44,7 @@ class HostContextVarMiddleware:
         if scope["type"] in {"http", "websocket"}:
             try:
                 request_info = RequestScopeInfo.parse_obj(scope)
-                logger.opt(colors=True).debug(f"Request info: <e>{request_info}</e>")
+                logger.opt(colors=True).trace(f"Request info: <e>{request_info}</e>")
             except Exception:
                 logger.exception(f"Scope {scope=} deserialize failed:")
             else:
@@ -95,3 +53,99 @@ class HostContextVarMiddleware:
 
 
 driver.server_app.add_middleware(HostContextVarMiddleware)
+driver.server_app.mount(
+    path="/filehost",
+    app=StaticFiles(directory=temporary_dir.name),
+    name="filehost",
+)
+
+
+def cleanup():
+    logger.debug(f"Program exit singal received, Start cleanup up {temporary_dir}")
+    temporary_dir.cleanup()
+
+
+atexit.register(cleanup)
+
+
+@export_namespace
+class FileHost(BaseModel):
+    @staticmethod
+    def new(source: Union[BytesIO, BufferedReader, Path, bytes, str]):
+        file_id = token_urlsafe()
+        tmpfile_path = Path(temporary_dir.name) / file_id
+
+        if isinstance(source, BytesIO):
+            content = source.getvalue()
+            tmpfile_path.write_bytes(content)
+        elif isinstance(source, bytes):
+            tmpfile_path.write_bytes(source)
+        else:
+            file_path = (
+                source
+                if isinstance(source, Path)
+                else Path(
+                    source.name if isinstance(source, BufferedReader) else str(source)
+                )
+            ).absolute()
+            if not file_path.is_file():
+                raise ValueError(f"Input source={source!r} is not a valid file!")
+            if not hosting_config.LINK_FILE:
+                shutil.copyfile(file_path, tmpfile_path)
+            else:
+                link_limit = (
+                    0 if hosting_config.LINK_FILE is True else hosting_config.LINK_FILE
+                )
+                if file_path.stat().st_size >= link_limit:
+                    try:
+                        if hosting_config.LINK_TYPE is LinkType.hard:
+                            file_path.link_to(tmpfile_path)
+                        else:
+                            file_path.symlink_to(tmpfile_path)
+                    except OSError as e:
+                        logger.opt(colors=True).warning(
+                            f"FileHost failed to create "
+                            f"<y>{hosting_config.LINK_TYPE.value}</y> link: "
+                            f"<r><b>{e.errno=}</b> {e.strerror}</r>, "
+                            "fallback to copy file."
+                        )
+                        shutil.copyfile(file_path, tmpfile_path)
+                else:
+                    shutil.copyfile(file_path, tmpfile_path)
+
+        return FileHost(id=file_id, path=tmpfile_path)
+
+    id: str
+    path: Path
+
+    class Config:
+        allow_mutation = False
+        extra = Extra.forbid
+
+    def to_url(self) -> str:
+        request = HostContextVarMiddleware.current_request.get(None)
+        if (request is None) or ("host" not in request.headers):
+            if hosting_config.FALLBACK_HOST is None:
+                raise ValueError(
+                    "No fallback host specified, "
+                    "and current request has no host header"
+                )
+            base_url = urljoin(hosting_config.FALLBACK_HOST, f"/filehost/{self.id}")
+        else:
+            base_url = ParseResult(
+                scheme={
+                    "ws": "http",
+                    "wss": "https",
+                    "http": "http",
+                    "https": "https",
+                }.get(request.scheme, "http"),
+                netloc=request.headers["host"],
+                path=f"/filehost/{self.id}",
+                params="",
+                query="",
+                fragment="",
+            ).geturl()
+        return base_url
+
+
+__all__ = ["FileHost", "HostContextVarMiddleware"]
--- src/plugins/nonebot_plugin_filehost/config.py
+++ src/plugins/nonebot_plugin_filehost/config.py
@@ -1,12 +1,20 @@
-from typing import Optional
+from typing import Optional, Union
+from enum import Enum
 
 from nonebot.config import BaseConfig
 from pydantic.fields import Field
+from pydantic import PositiveInt, AnyHttpUrl
+
+
+class LinkType(str, Enum):
+    hard = "hard"
+    soft = "soft"
 
 
 class Config(BaseConfig):
-    # Your Config Here
-    FALLBACK_HOST: Optional[str] = Field(alias="filehost_fallback_host")
+    FALLBACK_HOST: Optional[AnyHttpUrl] = Field(alias="filehost_fallback_host")
+    LINK_FILE: Union[PositiveInt, bool] = Field(True, alias="filehost_link_file")
+    LINK_TYPE: LinkType = Field(LinkType.hard, alias="filehost_link_type")
 
     class Config:
         extra = "ignore"
