4f0b3a856270ec770ee3e90e7f2a0a0f8bdc1aa7
Mix <mnixry@users.noreply.github.com>
2021-09-20 14:41:35 +0800

    :art: Format code with `black`

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import nonebot
 from nonebot.adapters.cqhttp import Bot as CQHTTPBot
 
 # Custom your logger
-# 
+#
 # from nonebot.log import logger, default_format
 # logger.add("error.log",
 #            rotation="00:00",
@@ -23,11 +23,13 @@ driver.register_adapter("cqhttp", CQHTTPBot)
 nonebot.load_from_toml("pyproject.toml")
 
 # Modify some config / config depends on loaded configs
-# 
+#
 # config = driver.config
 # do something...
 
 
 if __name__ == "__main__":
-    nonebot.logger.warning("Always use `nb run` to start the bot instead of manually running!")
+    nonebot.logger.warning(
+        "Always use `nb run` to start the bot instead of manually running!"
+    )
     nonebot.run(app="__mp_main__:app")
