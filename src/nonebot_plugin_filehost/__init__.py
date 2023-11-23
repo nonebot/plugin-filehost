45692c2b602229f27943e2d603033610e9093b18
Mix <32300164+mnixry@users.noreply.github.com>
2023-11-23 20:43:24 +0800

    :building_construction: Update project file structure

--- src/nonebot_plugin_filehost/__init__.py
+++ src/nonebot_plugin_filehost/__init__.py
@@ -13,7 +13,6 @@ from fastapi.staticfiles import StaticFiles
 from nonebot import get_driver
 from nonebot.drivers import ReverseDriver
 from nonebot.log import logger
-from nonebot.plugin.export import export
 from pydantic import BaseModel, Extra
 from starlette.types import ASGIApp, Receive, Scope, Send
 
@@ -23,16 +22,15 @@ from .models import RequestScopeInfo
 driver = get_driver()
 
 
-if not isinstance(driver, ReverseDriver) or not isinstance(driver.server_app, FastAPI):
+if not isinstance(driver, ReverseDriver) or not isinstance(driver, FastAPI):
     raise ValueError("FileHost supports FastAPI driver only")
 
 hosting_config = Config.parse_obj(driver.config.dict())
-export_namespace = export()
+
 
 temporary_dir = TemporaryDirectory(prefix="filehost-")
 
 
-@export_namespace
 class HostContextVarMiddleware:
     current_scope: ContextVar[Scope] = ContextVar("current_scope")
     current_request: ContextVar[RequestScopeInfo] = ContextVar("current_request")
